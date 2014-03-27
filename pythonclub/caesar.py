ad5e10a2e1c4f672a9f0060d778b4766b7177de5
jinkerry <jinkerry@126.com>
2014-03-27 20:08:49 +0800

    修改变量名，s未定义

--- pythonclub/caesar.py
+++ pythonclub/caesar.py
@@ -8,7 +8,7 @@ def caesar():
               "bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. " \
               "sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
     t = string.maketrans('abcdefghijklmnopqrstuvwzyz', 'cdefghijklmnopqrstuvwzyzab')
-    print s.translate(t)
+    print original.translate(t)
 
 if __name__ == '__main__':
     caesar()
